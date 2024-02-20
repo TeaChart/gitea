6326ef7ad0f4ed1b7e245cd055600fee8995c822
yp05327 <576951401@qq.com>
2024-02-20 14:39:50 +0900

    convert .Values.TeaChart into .TeaChart

--- templates/scripts/runner/configure_runner.sh
+++ templates/scripts/runner/configure_runner.sh
@@ -8,6 +8,6 @@ act_runner generate-config > $CONFIG_FILE
 
 # set the container network
 l=$(grep -n 'network:' $CONFIG_FILE | cut -d ':' -f1)
-sed -i "${l}s/.*/  network: \"{{ .Values.TeaChart.ProjectName }}_gitea\"/" $CONFIG_FILE
+sed -i "${l}s/.*/  network: \"{{ .TeaChart.ProjectName }}_gitea\"/" $CONFIG_FILE
 
 echo '==== END GITEA RUNNER CONFIGURATION ===='
